08ab1a7f1726520b7db5664804b3b6827363b464
Luan Antony <fplpgluan@gmail.com>
2024-12-11 15:20:21 -0300

    formatado o texto de todos os slides, adicionado a função dos botões e estilos de transições

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,45 @@
 const botaoPrincipal = document.querySelector('.botao-principal');
 const countryList = document.querySelector('.country-list');
 
+const nextBtn = document.getElementById("next-btn");
+const prevBtn = document.getElementById("prev-btn");
+const slides = document.querySelectorAll('.slide');
+const numberOfSlides = slides.length;
+
+
+let slideNumber = 0;
+
+nextBtn.onclick = () => {
+    if (slideNumber >= numberOfSlides - 1) {
+        return;
+    }
+
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber++;
+
+    slides[slideNumber].classList.add('active');
+};
+
+prevBtn.onclick = () => {
+    if (slideNumber <= 0) {
+        return;
+    }
+
+    slides.forEach((slide) => {
+        slide.classList.remove('active');
+    });
+
+    slideNumber--;
+
+    slides[slideNumber].classList.add('active');
+};
+
+
+
+//Menu de paises
 botaoPrincipal.addEventListener('click', () => {
     countryList.classList.toggle('hidden');
     botaoPrincipal.classList.toggle('active');
